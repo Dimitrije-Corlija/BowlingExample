41e1942a0dbc8b399c932aa93576774323e55fc3
DESKTOP-38TO0KG\PC <corlija.dimitrije822@gmail.com>
2021-11-02 11:49:27 +0100

    Prvi komit.

---
+++ BowlingExampleTemplate/Program.cs
@@ -0,0 +1,13 @@
+﻿using BowlingExampleTemplate.Bowling;
+using System;
+
+namespace BowlingExampleTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets play a bowling game...");
+        }
+    }
+}
